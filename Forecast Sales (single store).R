07c96b16e11d13ec1ef8d32ed02356ba228e5d66
vuongchu92 <42327886+vuongchu92@users.noreply.github.com>
2018-08-27 11:26:51 +0700

    Update Forecast Sales (single store).R

--- Forecast Sales (single store).R
+++ Forecast Sales (single store).R
@@ -5,6 +5,7 @@ library(forecast)
 library(xts)
 library(zoo)
 library(seasonal)
+library(imputeTS)
 
 data <- read.csv("train.csv",header=TRUE)
 
@@ -12,7 +13,7 @@ data <- read.csv("train.csv",header=TRUE)
 data1 <- data %>% group_by(store_id, date) %>% 
   summarise(Amount = sum(amount))
 
-data1$date <- as.Date(data1$date)
+data1$date <- as.Date(data1$date, "%m/%d/%Y")
 
 data2 <- data1 %>% dplyr:: filter(store_id == 0) ## Forecast sales for Store_ID = 0
 
@@ -23,15 +24,18 @@ data2 <- data2[,-1]
 
 data_with_missing_times <- full_join(df,data2)
 
-data_with_missing_times <-data_with_missing_times[6:length(data_with_missing_times),] %>% group_by(date) %>% mutate_each(funs(ifelse(is.na(.),0,.)))
+data_with_missing_times <-data_with_missing_times[6:nrow(data_with_missing_times),] %>% group_by(date) %>% mutate_each(funs(ifelse(is.na(.),0,.)))
 
 data_with_missing_times$Amount <- log(data_with_missing_times$Amount)
 
 tsdata <- xts(data_with_missing_times$Amount, order.by=as.POSIXlt(data_with_missing_times$date,tryFormats = c("%y %m %d")))
 
+tsdata <- na.remove(ma(tsdata, 7))
+
+tsdata <- ts(tsdata, c(2016,12,22), frequency = 365)
+
 autoplot(tsdata)+ggtitle("Card Sales")+xlab("Date")+ylab("Thousands") ## Data Plot
 
-tsdata <- ts(tsdata, c(2016,12,19), frequency = 365)
 train.data <- subset(tsdata,end=length(tsdata) - 50)
 test.data <- subset(tsdata,start=length(tsdata) - 50)
 
@@ -44,12 +48,9 @@ result2 <- forecast(fit2,h=51)
 result3 <- forecast(fit3,h=51)
 combination <- (result1$mean + result2$mean + result3$mean)/3
 accuracy(combination,test.data)
-forecast.data <- ts(c(result1$x,combination),c(2016,12,19), frequency = 365)
+forecast.data <- ts(c(result1$x,combination),c(2016,12,22), frequency = 365)
 
 autoplot(tsdata, series="Original") +
   autolayer(forecast.data, series="Forecast") +
   scale_colour_manual(
     values=c(`Forecast`="red",`Original`="blue"))
-
-
-
